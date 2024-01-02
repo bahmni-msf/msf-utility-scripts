5543a2cc140f624778adbf877bad5e966aff41e5
venkybavisetti <bavisetti.venkatesh@thoughtworks.com>
2024-01-02 12:01:40 +0530

    load script from msf-utils

--- bahmni-lite/setArtifactVersion.sh
+++ bahmni-lite/setArtifactVersion.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-verifyReleaseVersion(){
-  version=$1
-  tagCount=$(curl -s https://api.github.com/repos/$GITHUB_REPOSITORY/tags | jq --arg tagName "$version"  '[.[] | select( .name == $tagName)] | length')
-  if [ $tagCount -gt 0 ]; then
-    echo "Error: Version $version already released. Please update your version in package/.appversion"
-    exit 1
-  fi
-}
-
 setArtifactVersion(){
   version=$1
   echo "Setting version $version"
@@ -19,18 +10,12 @@ setArtifactVersion(){
 case $GITHUB_REF in
   refs/tags/*)
       echo "Current action is for tag.."
-      setArtifactVersion "$GITHUB_REF_NAME"
-      ;;
-  refs/heads/release-*)
-      echo "Current action is for release branch.."
-      version=$(echo $GITHUB_REF_NAME | cut -d '-' -f 2)
-      verifyReleaseVersion "$version"
-      setArtifactVersion "$version"-rc
+      echo "tag====$GITHUB_REF_NAME"
+      setArtifactVersion "$(cat package/.appversion)-$GITHUB_REF_NAME"
       ;;
   *)
-      echo "Current action is neither tag nor release branch.."
+      echo "Current action is not for tag.."
       version=$(cat package/.appversion)
-      verifyReleaseVersion "$version"
       setArtifactVersion "$version-$GITHUB_RUN_NUMBER"
       ;;
 esac
