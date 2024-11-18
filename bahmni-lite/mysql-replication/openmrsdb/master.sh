583dc9e51cb80118ca13e935fe76ae077c11c626
RadhaKrishnan.S <radhakrishnan.sekar@thoughtworks.com>
2024-11-18 10:05:46 +0530

    MOBN-2526 | Add validations to check required varaibles and use System ENV variables (#2)
    * Add validations to check required varaibles and use System ENV variables
    
    * NIT

--- bahmni-lite/mysql-replication/openmrsdb/master.sh
+++ bahmni-lite/mysql-replication/openmrsdb/master.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-export MYSQL_ROOT_USER=<Mysql_Root_Username>
-export MYSQL_ROOT_PASSWORD=<Mysql_Root_Password>
-export MYSQL_SLAVE_USER=<Replication_User>
-export MYSQL_SLAVE_PASSWORD=<Replication_User_Password>
+# Check if .env file path is provided
+if [ -z "$1" ]; then
+    echo "Error: No .env file path provided."
+    echo "Usage: $0 <path_to_env_file>"
+    exit 1
+fi
 
-until docker exec bahmni-lite-openmrsdb-1 sh -c "export MYSQL_PWD=$MYSQL_ROOT_PASSWORD; mysql -u root -e ';'"
+BAHMNI_DOCKER_ENV_FILE=$1
+source ${BAHMNI_DOCKER_ENV_FILE}
+
+# NOTE: Make sure to add the below variables in the shell configuration files like .bashrc, .zshrc
+# MYSQL_REPLICATION_USER - Replication_User(Created in Master)
+# MYSQL_REPLICATION_PASSWORD - Replication_User_Password(Created in Master)
+# MYSQL_ROOT_USER - Mysql_Root_Username(Slave root username)
+# MYSQL_ROOT_PASSWORD - Mysql_Root_Password(Slave root password)
+
+required_vars=("MYSQL_REPLICATION_USER" "MYSQL_REPLICATION_PASSWORD" "MYSQL_ROOT_USER" "MYSQL_ROOT_PASSWORD")
+# Check each variable
+for var in "${required_vars[@]}"; do
+    if [[ -z "${!var}" ]]; then
+        echo "Error: $var is not set or is empty."
+        exit 1
+    fi
+done
+
+until docker exec bahmni-lite-openmrsdb-1 sh -c "export MYSQL_PWD='$MYSQL_ROOT_PASSWORD'; mysql -u root -e ';'"
 do
     echo "Waiting for bahmni-lite-openmrsdb-1 database connection..."
     sleep 4
 done
 
-create_slave_user="delete from mysql.user where User='$MYSQL_SLAVE_USER';FLUSH PRIVILEGES;CREATE USER '$MYSQL_SLAVE_USER'@'%' IDENTIFIED BY '$MYSQL_SLAVE_PASSWORD'; GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_SLAVE_USER'@'%'; FLUSH PRIVILEGES; SHOW MASTER STATUS \\G;"
+create_slave_user="delete from mysql.user where User='$MYSQL_REPLICATION_USER';FLUSH PRIVILEGES;CREATE USER '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD'; GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%'; FLUSH PRIVILEGES; SHOW MASTER STATUS \\G;"
 
 create_slave_user_cmd="export MYSQL_PWD=\"$MYSQL_ROOT_PASSWORD\"; mysql -u $MYSQL_ROOT_USER -e \"$create_slave_user\""
 
